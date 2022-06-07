7adb51a95f7a83c4e6c508d063fd4ba787b682ee
tatendabakozw <tatendabakozw@gmail.com>
2022-06-07 15:44:25 +0200

    fix: added anaytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,34 @@
-import "../styles/globals.css";
+import { useEffect } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { StoreProvider } from "../Context/Store";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtab";
+import Script from "next/script";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    router.events.on("hashChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+      router.events.off("hashChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
-    <ChakraProvider>
-      <StoreProvider>
-        <Component {...pageProps} />
-      </StoreProvider>
-    </ChakraProvider>
+    <>
+      <ChakraProvider>
+        <StoreProvider>
+          <Component {...pageProps} />
+        </StoreProvider>
+      </ChakraProvider>
+    </>
   );
 }
 
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,12 +1,36 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
+import { GA_TRACKING_ID } from "../lib/gtab";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <Script
+            id="gtag-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap"
             rel="stylesheet"
